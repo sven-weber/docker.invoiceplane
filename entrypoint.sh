41b0fbfa8bf968a4226a1e5360f2d81af98ff8b7
sven-weber <mail@sven-weber.net>
2020-03-03 19:22:59 +0100

    Updating script with correct php binding

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/ash
 set -e
 
 timestamp() {
@@ -13,6 +13,7 @@ echo "Setting ${INVOICEPLANE_URL} as InvoicePlane Url".
 sed -i "s|^IP_URL=.*|IP_URL=http://${INVOICEPLANE_URL}|" ${INVOICEPLANE_CONF}
 
 #Start InvoicePlane
-command="php -S ${INVOICEPLANE_URL} index.php"
+PARSED_PORT="$(echo ${INVOICEPLANE_URL} | sed -nr 's,.*(:[0-9]+).*,\1,p')"
+command="php -S 0.0.0.0$PARSED_PORT index.php"
 echo "Starting server, $command"
 exec $command
